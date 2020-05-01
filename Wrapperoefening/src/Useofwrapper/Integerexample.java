f813b39e3cfd454a161df00592de519a7de9229e
siriprasan <siridiya@gmail.com>
2020-05-01 16:03:12 +0200

    learning:to test git repository

---
+++ Wrapperoefening/src/Useofwrapper/Integerexample.java
@@ -0,0 +1,13 @@
+package Useofwrapper;
+
+public class Integerexample {
+
+	public static void main(String[] args) {
+		Integer intObject= Integer.valueOf(5);
+		System.out.println(intObject);
+		int intPrimitive=intObject.intValue();
+		System.out.println(intPrimitive);
+
+	}
+
+}
